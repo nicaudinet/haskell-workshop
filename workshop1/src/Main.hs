e54a44f432f86db8bb279b92d46a029992c9dd9f
Mats Rauhala <mats.rauhala@relexsolutions.com>
2019-03-27 15:07:01 +0200

    Example application
    - Does not have nice model yet
    - Does not have documentation yet

--- workshop1/src/Main.hs
+++ workshop1/src/Main.hs
@@ -1,4 +1,33 @@
 module Main where
 
+import           Data.List.Split    (splitOn)
+import           Data.Maybe         (mapMaybe)
+import           System.Environment (getArgs)
+import           Text.Read          (readMaybe)
+
+data Candidate =
+  Candidate { constituency :: String
+            , party        :: String
+            , sex          :: String
+            , averageAge   :: Double }
+               deriving Show
+
+parser :: String -> Maybe Candidate
+parser line = case splitOn ',' line of
+                   [c,p,s,a] -> Just (Candidate (read c) (read p) (read s) (read a))
+                   _ -> Nothing
+
+fullParser :: String -> [Candidate]
+fullParser = mapMaybe parser . tail . lines
+
+handler :: FilePath -> IO ()
+handler path = do
+  f <- readFile path
+  mapM_ print (fullParser f)
+
 main :: IO ()
-main = putStrLn "Hello world!"
+main = do
+  args <- getArgs
+  case args of
+       [file] -> handler file
+       _      -> putStrLn "Call with FILE"
